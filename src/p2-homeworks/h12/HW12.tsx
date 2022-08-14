e2909b457bddb528a054bb286880618148239327
Dima <d2.grinevich@gmail.com>
2022-08-14 17:25:06 +0300

    added

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,20 +1,32 @@
 import React from "react";
 import s from "./HW12.module.css";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import {changeThemeC} from "./bll/themeReducer";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-
+    // const theme = 'some'; // useSelector
+    const theme = useSelector<AppStoreType,string>(state => state.themes.color)
+    console.log(theme)
+    const dispatch = useDispatch()
     // useDispatch, onChangeCallback
-
+    const onChangeCallback = (value : string)=>{
+        dispatch(changeThemeC(value))
+    }
     return (
         <div className={s[theme]}>
             <hr/>
             <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
-
+            <SuperSelect
+                options={themes}
+                onChangeOption={onChangeCallback}
+                value = {theme}
+            />
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
 
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
-const initState = {
 
+export type initStateType = {
+    color : string
+}
+
+const initState:initStateType = {
+    color : 'some'
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+const CHANGE_COLOR = "CHANGE_COLOR"
+type changeThemeCAC = {
+    type : "CHANGE_COLOR",
+    color : string
+}
+
+export const themeReducer = (state = initState, action: changeThemeCAC): initStateType => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case CHANGE_COLOR: {
+            return {
+                ...state,
+                color : action.color
+            };
         }
         default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export const changeThemeC = (color : string): changeThemeCAC => ({type : CHANGE_COLOR,color}); // fix any
